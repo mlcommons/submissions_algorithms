f381548d4688a0486edfaa953bc42e628269359a
David Tweedle <dtweedle@d-matrix.ai>
2025-09-24 13:46:28 -0400

    switch to jitted train step

--- submissions/self_tuning/ademamix/submission.py
+++ submissions/self_tuning/ademamix/submission.py
@@ -217,120 +217,154 @@ def tree_bias_correction(moment, decay, count):
   return jax.tree_util.tree_map(
       lambda t: t / bias_correction_.astype(t.dtype), moment)
 
-@functools.partial(
-        jax.pmap,
-        axis_name='batch',
-        in_axes=(None, None, 0, 0, 0, 0, 0, None, None),
-        static_broadcasted_argnums=(0, 1),
-        donate_argnums=(2, 3, 4)
-        )
-def pmapped_train_step(
-        workload,
-        opt_update_fn,
-        model_state,
-        optimizer_state,
-        current_param_container,
+#@functools.partial(
+#        jax.pmap,
+#        axis_name='batch',
+#        in_axes=(None, None, 0, 0, 0, 0, 0, None, None),
+#        static_broadcasted_argnums=(0, 1),
+#        donate_argnums=(2, 3, 4)
+#        )
+def train_step(workload,
+               opt_update_fn,
+               model_state,
+               optimizer_state,
+               current_param_container,
+               batch,
+               rng,
+               grad_clip,
+               label_smoothing,
+               dropout_rate,
+               ):
+
+  def _loss_fn(params):
+    """Loss function used for training."""
+    logits, new_model_state = workload.model_fn(
+        params,
         batch,
+        model_state,
+        spec.ForwardPassMode.TRAIN,
         rng,
-        grad_clip,
-        label_smoothing
-        ):
-    def _loss_fn(params):
-        logits, new_model_state = workload.model_fn(
-                params,
-                batch,
-                model_state,
-                spec.ForwardPassMode.TRAIN,
-                rng,
-                update_batch_norm=True,
-                )
-        loss_dict = workload.loss_fn(
-                label_batch=batch['targets'],
-                logits_batch=logits,
-                mask_batch=batch.get('weights'),
-                label_smoothing=label_smoothing,
-                )
-        summed_loss = loss_dict['summed']
-        n_valid_examples = loss_dict['n_valid_examples']
-        return summed_loss, (n_valid_examples, new_model_state)
+        update_batch_norm=True,
+        dropout_rate=dropout_rate,
+        )
+    loss_dict = workload.loss_fn(
+        label_batch=batch['targets'],
+        logits_batch=logits,
+        mask_batch=batch.get('weights'),
+        label_smoothing=label_smoothing)
+    summed_loss = loss_dict['summed']
+    n_valid_examples = loss_dict['n_valid_examples']
+    return summed_loss, (n_valid_examples, new_model_state)
 
-    grad_fn = jax.value_and_grad(_loss_fn, has_aux=True)
-    (summed_loss, (n_valid_examples, new_model_state)), grad = grad_fn(
-            current_param_container
-            )
-    (summed_loss, n_valid_examples, grad) = lax.psum(
-            (summed_loss, n_valid_examples, grad), axis_name='batch'
-            )
-    loss = summed_loss / n_valid_examples
-    grad = jax.tree.map(lambda x: x / n_valid_examples, grad)
-    grad_norm = jnp.sqrt(
-            sum(jnp.sum(g ** 2) for g in jax.tree_util.tree_leaves(grad))
-            )
-    if grad_clip is not None:
-        grad_scaling_factor = grad_clip / (grad_norm + _GRAD_CLIP_EPS)
-        grad_scaling_factor = jax.lax.clamp(min=0.0, x=grad_scaling_factor, max=1.0)
-        grad = jax.tree.map(lambda x: x * grad_scaling_factor, grad)
+  grad_fn = jax.value_and_grad(_loss_fn, has_aux=True)
+  (summed_loss, (n_valid_examples, new_model_state)), grad = grad_fn(
+      current_param_container)
+  # Get correct global mean loss and grad.
+  loss = summed_loss / n_valid_examples
+  grad = jax.tree.map(lambda x: x / n_valid_examples, grad)
 
-    updates, new_optimizer_state = opt_update_fn(
-            grad, optimizer_state, current_param_container
-            )
-    updated_params = optax.apply_updates(current_param_container, updates)
-    return new_optimizer_state, updated_params, new_model_state, loss, grad_norm
+  grad_norm = jnp.sqrt(
+      sum(jnp.sum(g**2) for g in jax.tree_util.tree_leaves(grad)))
+
+  if grad_clip is not None:
+    grad_scaling_factor = grad_clip / (grad_norm + _GRAD_CLIP_EPS)
+    grad_scaling_factor = jax.lax.clamp(min=0.0 x=grad_scaling_factor, max=1.0)
+    grad = jax.tree.map(lambda x: x * grad_scaling_factor, grad)
+
+  updates, new_optimizer_state = opt_update_fn(grad, optimizer_state,
+                                               current_param_container)
+  updated_params = optax.apply_updates(current_param_container, updates)
+  return new_optimizer_state, updated_params, new_model_state, loss, grad_norm
+
 
 def update_params(
-        workload: spec.Workload,
-        current_param_container: spec.ParameterContainer,
-        current_params_types: spec.ParameterTypeTree,
-        model_state: spec.ModelAuxiliaryState,
-        hyperparameters: spec.Hyperparameters,
-        batch: Dict[str, spec.Tensor],
-        loss_type: spec.LossType,
-        optimizer_state: spec.OptimizerState,
-        eval_results: List[Tuple[int, float]],
-        global_step: int,
-        rng: spec.RandomState,
-        train_state: Optional[Dict[str, Any]] = None,
-        ) -> spec.UpdateReturn:
-    del current_params_types
-    del loss_type
-    del train_state
-    del eval_results
-    del hyperparameters
+    workload: spec.Workload,
+    current_param_container: spec.ParameterContainer,
+    current_params_types: spec.ParameterTypeTree,
+    model_state: spec.ModelAuxiliaryState,
+    hyperparameters: spec.Hyperparameters,
+    batch: Dict[str, spec.Tensor],
+    loss_type: spec.LossType,
+    optimizer_state: spec.OptimizerState,
+    eval_results: List[Tuple[int, float]],
+    global_step: int,
+    rng: spec.RandomState,
+    train_state: Optional[Dict[str, Any]] = None) -> spec.UpdateReturn:
+  """Return (updated_optimizer_state, updated_params, updated_model_state)."""
+  del current_params_types
+  del loss_type
+  del train_state
+  del eval_results
+  del hyperparameters
 
-    hyperparameters = HPARAMS
+  hyperparameters = HPARAMS
 
-    optimizer_state, opt_update_fn = optimizer_state
-    per_device_rngs = jax.random.split(rng, jax.local_device_count())
-    if hasattr(hyperparameters, 'label_smoothing'):
-        label_smoothing = hyperparameters['label_smoothing']
-    else:
-        label_smoothing = 0.0
-    if hasattr(hyperparameters, 'grad_clip'):
-        grad_clip = hyperparameters['grad_clip']
-    else:
-        grad_clip = None
-        outputs = pmapped_train_step(
-                workload,
-                opt_update_fn,
-                model_state,
-                optimizer_state,
-                current_param_container,
-                batch,
-                per_device_rngs,
-                grad_clip,
-                label_smoothing,
-                )
-        new_optimizer_state, new_params, new_model_state, loss, grad_norm = outputs
+  optimizer_state, opt_update_fn = optimizer_state
+  per_device_rngs = jax.random.split(rng, jax.local_device_count())
+  if hasattr(hyperparameters, 'label_smoothing'):
+    label_smoothing = hyperparameters['label_smoothing']
+  else:
+    label_smoothing = 0.0
+  if hasattr(hyperparameters, 'grad_clip'):
+    grad_clip = hyperparameters['grad_clip']
+  else:
+    grad_clip = None
+  dropout_rate = hyperparameters.dropout_rate
+  
+  mesh = jax.sharding.Mesh(jax.devices(), ('batch'))
+  replicated = jax_sharding_utils.get_replicate_sharding(
+          mesh
+          )
+  sharded = jax_sharding_utils.get_batch_sharding(
+          mesh
+          )
+  arg_shardings = (
+          replicated, #model_state
+          replicated, #optimizer_state # change to optimizer sharding eventually
+          replicated, # current_param_container
+          sharded, # batch
+          replicated, # per_device_rngs
+          replicated, # grad_clip
+          replicated, #label_smoothing
+          replicated, #dropout_rate
+          )
+  out_shardings = (
+          replicated, # new_optimizer_state # maybe sharded eventually
+          replicated, # updated_params
+          replicated, # new_model_state
+          replicated, # loss
+          replicated, # grad_norm
+          )
+  jitted_train_step = jax.jit(
+          train_step,
+          static_argnums=(0, 1),
+          donate_argnums=(2, 3, 4),
+          in_shardings=arg_shardings,
+          out_shardings=out_shardings,
+          )
+  outputs = jitted_train_step(workload,
+                              opt_update_fn,
+                              model_state,
+                              optimizer_state,
+                              current_param_container,
+                              batch,
+                              per_device_rngs,
+                              grad_clip,
+                              label_smoothing,
+                              dropout_rate,
+                              )
+  new_optimizer_state, new_params, new_model_state, loss, grad_norm = outputs
 
-        if global_step % 100 == 0 and workload.metrics_logger is not None:
-            workload.metrics_logger.append_scalar_metrics(
-                    {
-                        'loss': loss[0],
-                        'grad_norm': grad_norm[0],
-                        },
-                    global_step
-                    )
-        return (new_optimizer_state, opt_update_fn), new_params, new_model_state
+  # Log loss, grad_norm.
+  if global_step % 100 == 0 and workload.metrics_logger is not None:
+    workload.metrics_logger.append_scalar_metrics(
+        {
+            'loss': loss[0],
+            'grad_norm': grad_norm[0],
+        }, global_step)
+  return (new_optimizer_state, opt_update_fn), new_params, new_model_state
+
+
 
 def prepare_for_eval(
         workload: spec.Workload,
