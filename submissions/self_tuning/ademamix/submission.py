5b9382a7bc6f21e8683784383a79a9baa9ce3071
Priya Kasimbeg <kasimbeg@google.com>
2026-03-25 04:10:26 +0000

    add bsz for fwedu

--- submissions/self_tuning/ademamix/submission.py
+++ submissions/self_tuning/ademamix/submission.py
@@ -352,6 +352,8 @@ def get_batch_size(workload_name):
     return 128
   elif workload_name == 'mnist':
     return 16
+  elif workload_name == 'finewebedu_lm':
+    return 64
   else:
     raise ValueError(f'Unsupported workload name: {workload_name}.')
 
